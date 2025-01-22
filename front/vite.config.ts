8e15fc2a03b24f1cdb41aeadaf5a4f8880d03d77
JeanManyama <jean.manyama@gmail.com>
2025-01-22 21:29:03 +0100

    vercel

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -6,7 +6,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   
   plugins: [react()],
-  base: './', 
+  base: '/', 
   // base:'/Kalhyge-prod/',
   // base: import.meta.env.VITE_BASE_URL ,
 
