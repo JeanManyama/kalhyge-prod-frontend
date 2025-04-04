06a1fe44dc7d8ff396195eb2dc249990f79d8d1a
JeanManyama <jean.manyama@gmail.com>
2025-04-04 15:24:26 +0200

    Modification de vercel.json

--- front/vite-env.d.ts
+++ front/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   VITE_BASE_URL: string;
-  // Ajoutez ici d'autres variables d'environnement que vous utilisez
+  // Ajoutez ici d'autres variables d'environnement que vous utilise
 }
 
 interface ImportMeta {
